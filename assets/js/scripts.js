1bbf8a997221296faea7a46180b35b99edbc9823
Frank-ssp <francivaldogomestavares634@gmail.com>
2021-12-21 12:44:34 -0400

    primeiro app java

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+console.log("Ola mundo");
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
