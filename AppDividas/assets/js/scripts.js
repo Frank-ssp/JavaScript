98a1ab497dfb9c686a3c8bcc9b999069243baf7c
Frank-ssp <francivaldogomestavares634@gmail.com>
2021-12-22 13:51:06 -0400

    alterando a função para inserir dois valores

--- AppDividas/assets/js/scripts.js
+++ AppDividas/assets/js/scripts.js
@@ -1,18 +1,25 @@
 const form = document.getElementById('task-form');
 const taskList = document.getElementById('tasks');
 
+
 form.onsubmit = function (e) {
 	e.preventDefault();
 	const inputField = document.getElementById('task-input');
-	addTask(inputField.value);
+	const inputField1 = document.getElementById('task-valor');
+	addTask(inputField.value,inputField1.value);
 	form.reset();
 };
 
-function addTask(description) {
+function addTask(description,description1) {
 	const taskContainer = document.createElement('div');
 	const newTask = document.createElement('input');
 	const taskLabel = document.createElement('label');
 	const taskDescriptionNode = document.createTextNode(description);
+
+
+	const taskLabel1 = document.createElement('label');
+	const taskDescriptionNode1 = document.createTextNode(description1);
+
 
 	newTask.setAttribute('type', 'checkbox');
 	newTask.setAttribute('name', description);
@@ -21,10 +28,14 @@ function addTask(description) {
 	taskLabel.setAttribute('for', description);
 	taskLabel.appendChild(taskDescriptionNode);
 
+	taskLabel1.setAttribute('for', description1);
+	taskLabel1.appendChild(taskDescriptionNode1);
+
 	taskContainer.classList.add('task-item');
 	taskContainer.appendChild(newTask);
 	taskContainer.appendChild(taskLabel);
 
-	taskList.appendChild(taskContainer);
+	taskContainer.appendChild(taskLabel1);
 
+	taskList.appendChild(taskContainer);
 }
